2e1e98e01e8fe12686a2a64f800dd4dfb833bb95
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-09-02 22:11:51 +0300

    Creating of the onion architecture

---
+++ Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+         => new() 
+         {
+             new() { Id=Guid.NewGuid(),Name="Telefon", Price=4500, Stock=10},
+             new() { Id=Guid.NewGuid(),Name="Tablet", Price=1200, Stock=10},
+             new() { Id=Guid.NewGuid(),Name="Televizyon", Price=3400, Stock=10},
+             new() { Id=Guid.NewGuid(),Name="Bilgisayar", Price=12000, Stock=10},
+             new() { Id=Guid.NewGuid(),Name="Kalemlik", Price=55, Stock=10},
+         };
+    }
+}
