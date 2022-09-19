b2d004323e478bb06b03046e483585e48bcd424d
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-09-19 20:48:54 +0300

    migration

---
+++ Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -0,0 +1,31 @@
+﻿using ETicaretAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Contexts
+{
+    public class ETicaretAPIDbContext:DbContext
+    {
+        //bu constructor IOC container da doldurulacak
+        public ETicaretAPIDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
--- Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -1,5 +1,5 @@
-﻿using ETicaretAPI.Application.Abstractions;
-using ETicaretAPI.Persistence.Concretes;
+﻿using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -14,7 +14,8 @@ namespace ETicaretAPI.Persistence
     {
         public static void AddPersistenceServices(this IServiceCollection services)
         {
-            services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseSqlServer("server=DESKTOP-EULMR9J;database=ETicaretAPIDb;" +
+                "integrated security=true;"));
         }
     }
 }
