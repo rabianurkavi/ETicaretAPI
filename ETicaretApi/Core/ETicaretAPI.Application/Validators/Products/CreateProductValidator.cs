ba1811f682cdab958019d29172023434419bc283
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-04-01 09:52:01 +0300

    ValidationFilter with Fluent Validation

---
+++ ETicaretApi/Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,34 @@
+﻿using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator:AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .NotNull()
+                   .WithMessage("Lütfen ürün adını boş geçmeyiniz.")
+                .MaximumLength(150)
+                .MinimumLength(3)
+                    .WithMessage("Lütfen ürün adını 3 ile 150 karakter giriniz.");
+            RuleFor(p => p.Stock)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Lütfen stock sayısını boş geşmeyiniz.")
+                .Must(s => s >= 0)//stock bilgisi 0 dan büyük ya da eşit olabilir
+                    .WithMessage("Stock sayısı 0 dan küçük olamaz!");
+            RuleFor(p => p.Price)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Lütfen fiyat bilgisini boş geşmeyiniz.")
+                .Must(p => p >= 0)//stock bilgisi 0 dan büyük ya da eşit olabilir
+                    .WithMessage("Fiyatı 0 dan küçük olamaz!");
+
+        }
+
+    }
+}
--- ETicaretApi/Presentation/ETicaretAPI.API/Program.cs
+++ ETicaretApi/Presentation/ETicaretAPI.API/Program.cs
@@ -1,4 +1,8 @@
+using ETicaretAPI.Application.Validators.Products;
+using ETicaretAPI.Infrastructure.Filters;
 using ETicaretAPI.Persistence;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +15,9 @@ builder.Services.AddCors(options=>options.AddDefaultPolicy(policy=>
   //.AllowAnyMethod()//bütün methodlara izin ver
   //.AllowAnyOrigin() //bütün originlere izin ver bu þekilde benim tüm apilerimý tüketebilir kötü sitelere izin vermiþ oluruz
 ));
-builder.Services.AddControllers();
+builder.Services.AddControllers(options=>options.Filters.Add<ValidationFilter>())
+       .AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())
+       .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter=true);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
